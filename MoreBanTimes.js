9216461a8d87c9195e5dee749a4c02eb56f28e3d
Cakery <Cakery@users.noreply.github.com>
2016-04-01 23:39:14 +1100

    Now with a date picker

--- MoreBanTimes.js
+++ MoreBanTimes.js
@@ -1,14 +1,24 @@
 // ==UserScript==
 // @name        MoreBanTimes
 // @namespace   MoreBan
-// @include     http://ADDYOURFORUMPAGEHERE/mcp.php?i=ban*
+// @include     http://ADDYOURFORUMHERE/mcp.php?i=ban*
 // @version     1
-// @grant       none
+// @grant       GM_addStyle
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     http://code.jquery.com/ui/1.11.4/jquery-ui.js
 // ==/UserScript==
+$("head").append (
+    '<link '
+  + 'href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/le-frog/jquery-ui.min.css" '
+  + 'rel="stylesheet" type="text/css">'
+);
 $(document).ready(function(){
   
       $("#banlength").prepend("<option value='5760'>4 Days</option>")
   $("#banlength").prepend("<option value='2880'>2 Days</option>")
 $("#banlength").prepend("<option value='15'>15 Minutes</option>")
+
+$("input[name='banlengthother']").datepicker({
+				dateFormat: "yy-mm-dd",
+})
 })
