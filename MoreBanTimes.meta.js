514f15945cdbf1e380e85b04b82041bf65adae30
Cakery <Cakery@users.noreply.github.com>
2016-04-02 01:35:48 +1100

    Create MoreBanTimes.meta.js

---
+++ MoreBanTimes.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        MoreBanTimes
+// @namespace   MoreBan
+// @include     http://aretegaming.com/mcp.php?i=ban*
+// @version     2
+// @grant       GM_addStyle
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     http://code.jquery.com/ui/1.11.4/jquery-ui.js
+// @downloadURL https://raw.githubusercontent.com/Cakery/MorebanTimes/master/MoreBanTimes.js
+// @updateURL   https://raw.githubusercontent.com/Cakery/MorebanTimes/master/MoreBanTimes.meta.js
+// ==/UserScript==
