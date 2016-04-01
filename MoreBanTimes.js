6a1beb2c0a4fa6bcdd993a3c169f275c92bb8fcc
Cakery <Cakery@users.noreply.github.com>
2016-04-02 00:16:19 +1100

    AutoUpdates?

--- MoreBanTimes.js
+++ MoreBanTimes.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        MoreBanTimes
 // @namespace   MoreBan
-// @include     http://ADDYOURFORUMHERE/mcp.php?i=ban*
-// @version     1
+// @include     http://aretegaming.com/mcp.php?i=ban*
+// @version     2
 // @grant       GM_addStyle
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     http://code.jquery.com/ui/1.11.4/jquery-ui.js
+// @downloadURL https://raw.githubusercontent.com/Cakery/MorebanTimes/master/MoreBanTimes.js
 // ==/UserScript==
 $("head").append (
     '<link '
