a6a82e6b860bbe43ccd1f8830e3207fc4f9fa117
Cakery <Cakery@users.noreply.github.com>
2016-04-02 18:27:02 +1100

    Create MoreBanTimes.user.js

---
+++ MoreBanTimes.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        MoreBanTimes
+// @namespace   MoreBan
+// @include     http://ADDSITEHERE/mcp.php?i=ban*
+// @version     2
+// @grant       GM_addStyle
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     http://code.jquery.com/ui/1.11.4/jquery-ui.js
+// @downloadURL https://raw.githubusercontent.com/Cakery/MorebanTimes/master/MoreBanTimes.js
+// ==/UserScript==
+$("head").append (
+    '<link '
+  + 'href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" '
+  + 'rel="stylesheet" type="text/css">'
+);
+$(document).ready(function(){
+  
+      $("#banlength").prepend("<option value='5760'>4 Days</option>")
+  $("#banlength").prepend("<option value='2880'>2 Days</option>")
+$("#banlength").prepend("<option value='15'>15 Minutes</option>")
+
+$("input[name='banlengthother']").datepicker({
+				dateFormat: "yy-mm-dd",
+})
+})
